898b2907603fbe7629be8c440173d0ef6daa9d95
deepspacediver <luka.koscik@gmail.com>
2021-12-24 09:23:46 +0100

    Styled the calculator, added math functions and click event on buttons

--- ReadME.md
+++ ReadME.md
@@ -5,3 +5,6 @@ pseudo code:
 create basic math functions
 create a function that operate on those basic functions
 create an interface of a calculator
+
+
+Function that when a button is clicked correspoding digit is shown in the input
---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("hello world")
+
+function add(n1, n2){
+    let sum = n1 + n2;
+   // console.log(sum)
+    return sum;  
+}
+
+function substract(n1, n2){
+    let substraction = n1 - n2;
+   // console.log(substraction)
+    return substraction;   
+}
+
+function multiply(n1, n2){
+    let multiplification = n1 * n2;
+    //console.log(multiplification)
+    return multiplification;  
+}
+
+function divide(n1, n2){
+    let division = n1 / n2;
+    //console.log(division)
+    return division;  
+}
+
+function operate(operator, n1, n2){
+    if (operator == "+"){
+        return add(n1, n2)
+    }
+    else if(operator == "-"){
+        return substract(n1, n2)
+    }
+    else if(operator =="*"){
+        return multiply(n1, n2)
+    }
+    else if(operator =="/"){
+        return divide(n1, n2)
+    }
+}
+
+
+
+let input = document.querySelector("input");
+
+let buttons = document.querySelector(".number-wrapper");
+
+buttons.addEventListener("click", (e) => {
+    console.log(e.target.innerText)
+})
