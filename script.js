7e393e7474cd8cc13183591d90d890becdabf2fa
deepspacediver <luka.koscik@gmail.com>
2021-12-27 12:51:07 +0100

    Added basic events populate input with equation

--- script.js
+++ script.js
@@ -2,42 +2,42 @@ console.log("hello world")
 
 function add(n1, n2){
     let sum = n1 + n2;
-   // console.log(sum)
+    input.value = sum;
     return sum;  
 }
 
 function substract(n1, n2){
     let substraction = n1 - n2;
-   // console.log(substraction)
+    input.value = substraction
     return substraction;   
 }
 
 function multiply(n1, n2){
     let multiplification = n1 * n2;
-    //console.log(multiplification)
+    input.value = multiplification;
     return multiplification;  
 }
 
 function divide(n1, n2){
     let division = n1 / n2;
-    //console.log(division)
+    input.value = division;
     return division;  
 }
 
-function operate(operator, n1, n2){
-    if (operator == "+"){
-        return add(n1, n2)
+    function operate(operator, n1, n2){
+        if (operator == "+"){
+            return add(n1, n2)
+        }
+        else if(operator == "-"){
+            return substract(n1, n2)
+        }
+        else if(operator =="*"){
+            return multiply(n1, n2)
+        }
+        else if(operator =="/"){
+            return divide(n1, n2)
+        }
     }
-    else if(operator == "-"){
-        return substract(n1, n2)
-    }
-    else if(operator =="*"){
-        return multiply(n1, n2)
-    }
-    else if(operator =="/"){
-        return divide(n1, n2)
-    }
-}
 
 
 
@@ -46,15 +46,23 @@ let input = document.querySelector("#input");
 let buttons = document.querySelector(".number-wrapper");
 let firstUserNumber;
 buttons.addEventListener("click", (e) => {
-    //console.log(e.target.innerText)
     input.value += e.target.innerText
-    firstUserNumber = input.value;
 })
 
+
+let operatorValue;
 let operators = document.querySelector(".operators-wrap")
-//console.log(operators)
 
-operators.addEventListener('click', (e) =>{
-   console.log(firstUserNumber )
-})
+    operators.addEventListener('click', (e) =>{
+    firstUserNumber = input.value;
+    operatorValue = e.target.innerText;
+    input.value = "";
+    })
 
+let secondUserNumber;
+let equalButton = document.getElementById('equalBtn');
+
+    equalButton.addEventListener('click', () => {
+        secondUserNumber = input.value;
+        operate(operatorValue, firstUserNumber,secondUserNumber)
+    })
